a59c0642a5d0306a39908a40e438fbcaf859fc98
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-08-15 11:31:48 +0200

    Source external refs from ACE_EXT_REFS env var

--- dashboard/components/how-to/cloudautomation.tsx
+++ dashboard/components/how-to/cloudautomation.tsx
@@ -1,11 +1,19 @@
 import { FunctionComponent } from 'react'
-import { CloudAutomationLink } from '../../components/credentials/cloudautomation'
+import { useExtRefs } from '../ext-refs/lib'
+import LinkTemplate from '../ext-refs/templates/LinkTemplate'
 
 type CloudAutomationProps = {}
 
-const CloudAutomation: FunctionComponent<CloudAutomationProps> = () =>
-	<div>
-		<p>Your <CloudAutomationLink /> has been specified when the ACE Box was launched.</p>
-	</div>
+const CloudAutomation: FunctionComponent<CloudAutomationProps> = () => {
+  const { findUrl } = useExtRefs()
+
+  const url = findUrl('CLOUD AUTOMATION')
+	
+	return (
+		<div>
+			<p>Your <LinkTemplate href={url} label='Cloud Automation' /> has been specified when the ACE Box was launched.</p>
+		</div>
+	)
+}
 
 export { CloudAutomation as default }
--- dashboard/components/how-to/keptn.tsx
+++ dashboard/components/how-to/keptn.tsx
@@ -1,13 +1,24 @@
 import { FunctionComponent } from 'react'
-import { KeptnBridgeLink, KeptnBridgeUsername, KeptnBridgePassword } from '../credentials/keptn-bridge'
+import { useExtRefs } from '../ext-refs/lib'
+import AceBoxCredentialInline from '../ext-refs/templates/CredentialInlineTemplate'
+import LinkTemplate from '../ext-refs/templates/LinkTemplate'
 
 type KeptnProps = {}
 
-const Keptn: FunctionComponent<KeptnProps> = () =>
-  <div>
-    <p><i>Keptn</i> will play a central role in most of the use cases. Keptn is a control-plane for DevOps automation of cloud-native applications. 
-    The <KeptnBridgeLink /> can be accessed using <KeptnBridgeUsername variant="inline" /> and password <KeptnBridgePassword variant="inline" />.</p>
-    <p>More infos can be found on <a href="https://keptn.sh" target="_blank" rel="noreferrer">keptn.sh</a>.</p>
-  </div>
+const Keptn: FunctionComponent<KeptnProps> = () => {
+  const { findUrl, findCreds } = useExtRefs()
+
+  const url = findUrl('KEPTN')
+  const username = findCreds('KEPTN', 'USERNAME')
+  const password = findCreds('KEPTN', 'PASSWORD')
+
+  return (
+    <div>
+      <p><i>Keptn</i> will play a central role in most of the use cases. Keptn is a control-plane for DevOps automation of cloud-native applications. 
+      The <LinkTemplate href={url} label='Keptn Bridge' /> can be accessed using <AceBoxCredentialInline value={username?.value} /> and password <AceBoxCredentialInline type='password' value={password?.value} />.</p>
+      <p>More infos can be found on <a href="https://keptn.sh" target="_blank" rel="noreferrer">keptn.sh</a>.</p>
+    </div>
+  )
+}
 
 export { Keptn as default }
