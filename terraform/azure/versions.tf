d81b1d07855f0b7f2d974ddd39e3802c925c94fc
eemrdog <dogan.emre@gmail.com>
2024-11-04 16:40:02 +0100

    azure rm provider disabled

--- terraform/azure/versions.tf
+++ terraform/azure/versions.tf
@@ -16,10 +16,11 @@ terraform {
   required_version = ">= 0.12.20"
 
   required_providers {
-    azurerm = {
+/*    azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.x"
     }
+*/
     random = "~> 3.3.2"
     local  = "~> 2.2.2"
   }
